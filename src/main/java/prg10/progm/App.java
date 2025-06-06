04fd000f10f94de2fd4b5dfcd01646afb0ee9e1e
Roopashree <r.pratahkal@gmail.com>
2025-06-06 12:06:11 +0530

    first commit

---
+++ src/main/java/prg10/progm/App.java
@@ -0,0 +1,13 @@
+package prg10.progm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
